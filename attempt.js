09d2859d68a1b8a3028da8c89a6f1ac8f5017551
Rika <60824200+mazsola739@users.noreply.github.com>
2021-06-11 09:43:19 +0200

    Create attempt.js

---
+++ attempt.js
@@ -0,0 +1,16 @@
+/*6. attempt
+
+This snippet executes a function, returning either the result or the caught error object.*/
+
+const attempt = (fn, ...args) => {
+  try {
+    return fn(...args);
+  } catch (e) {
+    return e instanceof Error ? e : new Error(e);
+  }
+};
+
+/*var elements = attempt(function(selector) {
+  return document.querySelectorAll(selector);
+}, '>_>');
+if (elements instanceof Error) elements = []; // elements = []*/
