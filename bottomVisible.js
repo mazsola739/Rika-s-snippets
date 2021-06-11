c0afc9c7d4e2cc70c0bc8e2b3bf25c438a0c6d22
Rika <60824200+mazsola739@users.noreply.github.com>
2021-06-11 09:47:32 +0200

    Create bottomVisible.js

---
+++ bottomVisible.js
@@ -0,0 +1,9 @@
+/*11. bottomVisible
+
+This snippet checks whether the bottom of a page is visible.*/
+
+const bottomVisible = () =>
+  document.documentElement.clientHeight + window.scrollY >=
+  (document.documentElement.scrollHeight || document.documentElement.clientHeight);
+
+//bottomVisible(); // true
